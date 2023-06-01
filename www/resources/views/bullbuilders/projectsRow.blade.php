be807a89337e4f26d70f265faf2c3b575083ba57
Morra13 <55277914+Morra13@users.noreply.github.com>
2023-06-02 00:17:27 +0400

    Проекты ублал лишнее

--- www/resources/views/bullbuilders/projectsRow.blade.php
+++ www/resources/views/bullbuilders/projectsRow.blade.php
@@ -3,13 +3,7 @@
         <div class="work img d-flex align-items-end" style="background-image: url({{ asset('storage') . '/' . $project['main_img']}});">
             <div class="desc w-100 px-4">
                 <div class="text w-100 mb-3">
-                    <span>{{ __($project['name']) }}</span>
-                    <h3>{{ __($project['manager']) }}</h3>
-                    @if($project['status'] == 'completed')
-                    <h4 class="btn-outline-success">{{ __('projects.completed') }}</h4>
-                    @elseif($project['status'] == 'incomplete')
-                        <h4 class="btn-outline-danger">{{ __('projects.incomplete') }}</h4>
-                    @endif
+                    <div>{{ __($project['name']) }}</div>
                 </div>
             </div>
         </div>
