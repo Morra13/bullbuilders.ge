cfac48ac86176b6caf0ca91955a417db55ff229e
Morra13 <55277914+Morra13@users.noreply.github.com>
2023-12-26 20:03:04 +0400

    слайдер размытие текста

--- www/resources/views/bullbuilders/slider.blade.php
+++ www/resources/views/bullbuilders/slider.blade.php
@@ -4,7 +4,7 @@
             <div class="container">
                 <div class="row no-gutters slider-text align-items-center justify-content-center">
                     <div class="col-md-12 ftco-animate">
-                        <div class="text w-100 text-center">
+                        <div class="text w-100 text-center slider-title">
                             <h2>{{ $slider['subtitle'] }}</h2>
                             <h1 class="mb-3">{{ $slider['title'] }}</h1>
                         </div>
